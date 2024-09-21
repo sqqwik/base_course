9155ebb2ef429a22539c1a20b4ef91112a238e6e
sqqwik <dolgovaa2312@gmail.com>
2024-09-21 10:04:01 +0000

    1 лекция

---
+++ lec_input.py
@@ -0,0 +1,9 @@
+a = input()
+print(a)
+
+a = input("Введите значение a: ")
+print(a)
+print (type(a))
+
+a = int(input("Введите значение а: "))
+print(type(a))
---
+++ task1.py
@@ -0,0 +1,7 @@
+print("Hello, world")
+
+print("Маша+Петя=Любовь")
+
+print("x = 3+4")
+
+print(3+4)
