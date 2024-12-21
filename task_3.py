b659886072b58c9069120a60059b044fa448763b
sqqwik <dolgovaa2312@gmail.com>
2024-12-21 08:46:51 +0000

    dfrgvbdgbd

--- task_3.py
+++ task_3.py
@@ -3,10 +3,10 @@ M = 5
 N = 10
 timer = time.time()
 for a in range (M+1):
-    print(f"Внешний цикл: , {a}")
+    print(f"Внешний цикл:  {a}")
     time.sleep(1)
     for i in range (N+1):
-        print(f"Внутренний цикл: , {i}")
+        print(f"Внутренний цикл:  {i}")
         time.sleep(1)
 
 print(f"Общее время: {time.time() - timer} секунд")
